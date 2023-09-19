c39e3e721f8ff229c9b3250d2d667e7f271401ae
Chad Scott <132615072+drchscott@users.noreply.github.com>
2023-09-19 15:00:20 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "The Main Idea",
-  "body": " The Main Idea  Consider the function   Notice that this function can't be evaluated at x=0 (i.e. 0 is not in its domain.) But, we could compute it for values of x *close* to 0. For example, consider the following table for sin(x)\/x.         0.500  0.958851    0.100  0.998334    0.010  0.999983    0.001  0.999999     Look at this table. As we move the values closer to , what are the values getting closer to? It's apparent that as approaches , that approaches . Of course, to be fair, at the moment we're only letting approach from the right hand (positive) side of . Let's create some notation to record this kind of information.  If approaches a number, say , as approaches a number from its right hand side, then we write which is pronounced \"The limit of is as approaches from the right\". We also say that has a right hand limit of at . Analogously, If approaches a number, say as approaches from its left hand side, then we write which is pronounced \"The limit of is as approaches from the left\". Again, we also say that has a left hand limit of at .  It is critical to note here that we are using the word \"approach\" in the definition above to indicate that we simply take values of the domain variable *close* to but not equal to it. In computing limits, we do not use . We're just identifying what is becoming as *approaches* . In fact, in many cases such as , we can't even compute the function at .  For from above, make a table like the one above but this time use 4 or 5 domain values approaching from the left. Use the notation from the definition above to write your conclusion about the left hand limit.  Finally, since it will often be the case that both the left hand and right hand limit come out the same (but not always) for a function, we just drop the and in that case. In other words ...  In case both the left hand and right hand limit of a function are the same (i.e. ), then we simply write without any reference to or . In this case we say that has a limit of at .  For , notice that once again is not in its domain. Make tables for left and right hand approaches to and use proper notation as defined above to write your conclusions about right hand limit, left hand limit and limit.  "
+  "body": " The Main Idea  Consider the function   Notice that this function can't be evaluated at x=0 (i.e. 0 is not in its domain.) But, we could compute it for values of x *close* to 0. For example, consider the following table for sin(x)\/x.         0.500  0.958851    0.100  0.998334    0.010  0.999983    0.001  0.999999     Look at this table. As we move the values closer to , what are the values getting closer to? It's apparent that as approaches , that approaches . Of course, to be fair, at the moment we're only letting approach from the right hand (positive) side of . Let's create some notation to record this kind of information.  If approaches a number, say , as approaches a number from its right hand side, then we write which is pronounced \"The limit of is as approaches from the right\". We also say that has a right hand limit of at . Analogously, If approaches a number, say as approaches from its left hand side, then we write which is pronounced \"The limit of is as approaches from the left\". Again, we also say that has a left hand limit of at .  So now, with this new notation, it seems reasonable to write   It is critical to note here that we are using the word \"approach\" in the definition above to indicate that we simply take values of the domain variable *close* to but not equal to it. In computing limits, we do not use . We're just identifying what is becoming as *approaches* . In fact, in many cases such as , we can't even compute the function at .  For from above, make a table like the one above but this time use 4 or 5 domain values approaching from the left. Use the notation from the definition above to write your conclusion about the left hand limit.  Finally, since it will often be the case that both the left hand and right hand limit come out the same (but not always) for a function, we just drop the and in that case. In other words ...  In case both the left hand and right hand limit of a function are the same (i.e. ), then we simply write without any reference to or . In this case we say that has a limit of at .  For , notice that once again is not in its domain. Make tables for left and right hand approaches to and use proper notation as defined above to write your conclusions about right hand limit, left hand limit and limit.  "
 },
 {
   "id": "sideLimit",
@@ -28,11 +28,20 @@ var ptx_lunr_docs = [
   "body": "If approaches a number, say , as approaches a number from its right hand side, then we write which is pronounced \"The limit of is as approaches from the right\". We also say that has a right hand limit of at . Analogously, If approaches a number, say as approaches from its left hand side, then we write which is pronounced \"The limit of is as approaches from the left\". Again, we also say that has a left hand limit of at . "
 },
 {
+  "id": "remark-1",
+  "level": "2",
+  "url": "mainIdea.html#remark-1",
+  "type": "Remark",
+  "number": "1.1.2",
+  "title": "",
+  "body": "It is critical to note here that we are using the word \"approach\" in the definition above to indicate that we simply take values of the domain variable *close* to but not equal to it. In computing limits, we do not use . We're just identifying what is becoming as *approaches* . In fact, in many cases such as , we can't even compute the function at . "
+},
+{
   "id": "example-1",
   "level": "2",
   "url": "mainIdea.html#example-1",
   "type": "Example",
-  "number": "1.1.2",
+  "number": "1.1.3",
   "title": "",
   "body": "For from above, make a table like the one above but this time use 4 or 5 domain values approaching from the left. Use the notation from the definition above to write your conclusion about the left hand limit. "
 },
@@ -41,7 +50,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "mainIdea.html#limit",
   "type": "Definition",
-  "number": "1.1.3",
+  "number": "1.1.4",
   "title": "",
   "body": "In case both the left hand and right hand limit of a function are the same (i.e. ), then we simply write without any reference to or . In this case we say that has a limit of at . "
 },
@@ -50,7 +59,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "mainIdea.html#example-2",
   "type": "Example",
-  "number": "1.1.4",
+  "number": "1.1.5",
   "title": "",
   "body": "For , notice that once again is not in its domain. Make tables for left and right hand approaches to and use proper notation as defined above to write your conclusions about right hand limit, left hand limit and limit. "
 },
@@ -115,7 +124,79 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.3",
   "title": "Limit Properties",
-  "body": " Limit Properties  Coming soon ...  "
+  "body": " Limit Properties  A careful observer will notice that in the previous sections, we only looked at a handful of domain values to essentially make a guess as to where the function is headed. Consider, for example, our friend . Look at the table we made at the beginning of the first section. We stopped looking once we let get down to . But what about even closer? Like or or ? Maybe waaaay down there starts getting wonky. Maybe. So, right now we're just guessing that . Educated guessing ... yes. But still just guessing. We'd like to start moving past just guessing. We start doing that in this section. We start by looking at a couple of functions whose limits are not in question.  First, consider a constant function like . No matter what you put in for , it gives us . So, there's no question what it's approaching regardless of where we let go. It's always giving . So, we have ...    If is constant, say for some number , and is any number, Then .    Secondly, consider the function . Well, this function just does whatever does. So, where goes, so does . Thus, we have ...    If and is any number, Then .    It is our good fortune that limits behave according to the following theorem.    If and and and are any two numbers and the right hand side of the following equalities are defined, Then , , and .    It is also the case that if all of the are replaced with either or , then the theorem is still correct.  Often, people cite this basic properties theorem by saying, \"the limit of a sum is the sum of the limits, the limit of a product is the product of the limits, and the limit of a ratio is the ratio of the limits\".  Please note that, from the conditions of the theorem, if the right hand side of any of the equalities in this theorem can not be computed, then the theorem is not making a statement (i.e. isn't valid, isn't a thing). For example, if in the theorem, then we certainly are not claiming that since the right hand side of this equality is which isn't even a thing to claim something is.  Use to compute , , and .  Using , we quickly deduce the following quick conclusions (called \"corollaries\" by mathematicians).      If is a nonzero rational number and is a nonzero number at which is defined, then .    If and are polynomials and , then .      Finally, we provide here a short list of known limits for use in subsequent sections.      If is any number, then .    If is any number, then .    If is any number, then .    If , then .      "
+},
+{
+  "id": "thm-limitOfConstants",
+  "level": "2",
+  "url": "limitProps.html#thm-limitOfConstants",
+  "type": "Theorem",
+  "number": "1.3.1",
+  "title": "",
+  "body": "  If is constant, say for some number , and is any number, Then .   "
+},
+{
+  "id": "thm-limitOfIdentity",
+  "level": "2",
+  "url": "limitProps.html#thm-limitOfIdentity",
+  "type": "Theorem",
+  "number": "1.3.2",
+  "title": "",
+  "body": "  If and is any number, Then .   "
+},
+{
+  "id": "thm-limitBasicProperties",
+  "level": "2",
+  "url": "limitProps.html#thm-limitBasicProperties",
+  "type": "Theorem",
+  "number": "1.3.3",
+  "title": "",
+  "body": "  If and and and are any two numbers and the right hand side of the following equalities are defined, Then , , and .   "
+},
+{
+  "id": "remark-2",
+  "level": "2",
+  "url": "limitProps.html#remark-2",
+  "type": "Remark",
+  "number": "1.3.4",
+  "title": "",
+  "body": "It is also the case that if all of the are replaced with either or , then the theorem is still correct. "
+},
+{
+  "id": "remark-3",
+  "level": "2",
+  "url": "limitProps.html#remark-3",
+  "type": "Remark",
+  "number": "1.3.5",
+  "title": "",
+  "body": "Please note that, from the conditions of the theorem, if the right hand side of any of the equalities in this theorem can not be computed, then the theorem is not making a statement (i.e. isn't valid, isn't a thing). For example, if in the theorem, then we certainly are not claiming that since the right hand side of this equality is which isn't even a thing to claim something is. "
+},
+{
+  "id": "example-6",
+  "level": "2",
+  "url": "limitProps.html#example-6",
+  "type": "Example",
+  "number": "1.3.6",
+  "title": "",
+  "body": "Use to compute , , and . "
+},
+{
+  "id": "limitCorollaries",
+  "level": "2",
+  "url": "limitProps.html#limitCorollaries",
+  "type": "Corollary",
+  "number": "1.3.7",
+  "title": "",
+  "body": "    If is a nonzero rational number and is a nonzero number at which is defined, then .    If and are polynomials and , then .     "
+},
+{
+  "id": "thm-aFewKnownLimits",
+  "level": "2",
+  "url": "limitProps.html#thm-aFewKnownLimits",
+  "type": "Theorem",
+  "number": "1.3.8",
+  "title": "",
+  "body": "    If is any number, then .    If is any number, then .    If is any number, then .    If , then .     "
 },
 {
   "id": "Continuity",
